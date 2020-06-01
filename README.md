64532256d63b0d220be4e2b2e8bcc9393fbc91a2
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-06-01 02:26:30 -0700

    final version

---
+++ README.md
@@ -0,0 +1,29 @@
+# README Generator
+The objective of this tool is to quickly provide a Readme for any repository.
+# Table of content
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licence](#licence)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+# Installation
+    npm install
+
+# Usage
+To use this tool, start by cloning the repository on your computer.
+Then make sure to be on the targeted folder and make sure to run npm install to get all the modules needed. You can now type "node index.js" in your terminal. You will be prompt with some questions to quickly create your README.
+
+![gif](./assets/usage.gif)
+# Licence
+No licence
+# Credits
+Not credits
+    
+# Questions
+![logo](https://avatars3.githubusercontent.com/u/59237050?v=4)
+- Hedi Calabrese
+- Email : Not defined
+
+## Main Language used in this repo              
+![badge](https://img.shields.io/badge/-JavaScript-blue)
--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+// 'use strict';
+// REQUIRE SECTION
 const inquirer = require(`inquirer`);
-
-const axios = require("axios")
-
+const axios = require("axios");
 const fs = require("fs");
+const util = require("util");
 
-const repos = []
+// MODULES FOR AUTOCOMPLETE FEATURE
+var _ = require('lodash');
+var fuzzy = require('fuzzy');
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
+// VARIABLES TO STORE USER REPOSITORY
+const repos = [];
+let repoUrl = "";
+const indexRepo = "";
+let userChoices = {};
+let badge = "";
+
+// FUNCTION FOR AUTOCOMPLETE FEATURE
+function searchRepos(answers, input) {
+    input = input || '';
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            var fuzzyResult = fuzzy.filter(input, repos);
+            resolve(
+                fuzzyResult.map(function (el) {
+                    return el.original;
+                })
+            );
+        }, _.random(30, 500));
+    });
+}
+
+// STARTING PROMPT TO GET USER CHOICES
 inquirer.prompt([
+    {
+        type: "input",
+        message: "Can you had your Github username?",
+        name: "username"
+    },
     {
         type: "input",
         message: "What is the title of your project?",
@@ -48,39 +80,30 @@ inquirer.prompt([
         message: "Please add the credits",
         name: "creditsInput",
         when: (answers) => answers.creditsConfirm === true,
-    },
-    {
-        type: "input",
-        message: "Can you had your Github username?",
-        name: "username"
-    },
+    }
 ])
     .then((response) => {
-        if (!response.creditsInput) {
-            response.creditsInput = "Not credits"
-        }
-        if (!response.licenceInput) {
-            response.licenceInput = "No licence"
-        }
-        console.log(response.title);
-        console.log(response.description);
-        console.log(response.usage);
-        console.log(response.licenceConfirm);
-        console.log(response.licenceInput);
-        console.log(response.creditsConfirm);
-        console.log(response.creditsInput);
-        console.log(response.username);
+        // SETTING VALUE IF NO INPUT FROM USER
+        !response.creditsInput ? response.creditsInput = "Not credits" : response.creditsInput;
+        !response.licenceInput ? response.licenceInput = "No licence" : response.licenceInput;
+
+        userChoices = response
+        return response
+
+    })
+    // CREATING INITIAL README FILE WITH FIRST SECTIONS
+    .then((response) => {
         fs.writeFile(`README.md`, `# ${response.title}
 ${response.description}
 # Table of content
-- Installation
-- Usage
-- Licence
-- Contributing
-- Questions
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licence](#licence)
+- [Contributing](#contributing)
+- [Questions](#questions)
 
 # Installation
-${response.installation}
+    ${response.installation}
 
 # Usage
 ${response.usage}
@@ -88,42 +111,99 @@ ${response.usage}
 ${response.licenceInput}
 # Credits
 ${response.creditsInput}
-        `
+    `
             , (error) => {
                 if (error) {
                     console.log(error);
                 }
-                console.log("Your readme has been created");
             })
-        axios.get(`https://api.github.com/users/${response.username}`)
-            .then(resp => {
-                console.log(resp.data.name)
-                console.log(resp.data.avatar_url)
-                console.log(resp.data.email)
-                if (!resp.data.email) {
-                    resp.data.email = "Not defined"
-                }
+        return response.username
+    }
+
+    )
+    // HTML REQUEST TO GET USER REPOS
+    .then((response) => axios
+        .get(`https://api.github.com/users/${userChoices.username}`)
+        .then((response) => {
+            repoUrl = response.data.repos_url
+            return repoUrl
+        }))
+    .then((response) => axios
+        .get(response)
+        .then((response) => {
+            const allRepos = response.data
+            allRepos.forEach((element) => {
+                repos.push(element.name)
+            });
+        }
+        ))
+    .then((response) => {
+        // GET USER CHOICE FOR THE TARGETED REPO (USING AUTOCOMPLETE) TO GENERATE THE BADGE
+        asyncInq(response)
+        // APPENDING THE CONTENT FOR QUESTION SECTION (User info, email and picture)
+        asyncAppend()
+    }).catch((err) => console.log(err))
+
+
+// ASYNC PROMPT TO LOAD REPOS FROM USER AND GENERATE BADGE
+const asyncInq = async () => {
+    try {
+        await inquirer.prompt([
+            {
+                type: 'autocomplete',
+                name: 'repositories',
+                message: 'Select a repository',
+                source: searchRepos,
+            }
+        ]).then((response) => {
+            const userRepo = repos.indexOf(response.repositories)
+
+            axios
+                .get(repoUrl)
+                .then((response) => {
+                    const language = response.data[userRepo].language
+                    badge = `https://img.shields.io/badge/-${language}-blue`
+                    return badge
+                })
+                .then(() => {
+                    fs.appendFile(`README.md`, `
+
+## Main Language used in this repo              
+![badge](${badge})`
+                        , (error) => {
+                            if (error) {
+                                console.log(error);
+                            }
+                        }
+                    )
+                })
+        })
+    } catch (err) {
+        console.log(err);
+    } console.log("Your readme has been created");
+}
+const asyncAppend = async () => {
+    try {
+        await axios
+            .get(`https://api.github.com/users/${userChoices.username}`)
+            .then(response => {
+                !response.data.email ? response.data.email = "Not defined" : response.data.email;
+                return response
+            })
+            .then(response => {
                 fs.appendFile(`README.md`, `
 # Questions
-![logo](${resp.data.avatar_url})
-- ${resp.data.name}
-- Email : ${resp.data.email}`
+![logo](${response.data.avatar_url})
+- ${response.data.name}
+- Email : ${response.data.email}`
                     , (error) => {
                         if (error) {
                             console.log(error);
                         }
-                        console.log("Your readme has been created");
-                    })
+                    }
+                )
             })
-            .catch(error => {
-                console.log(error);
-            });
-    });
-
-
-
-
-// github endpoint : https://api.github.com/
-// for user : https://api.github.com/users/username
-// for repo : https://api.github.com/repos/:owner/:repo/languages
-// example: https://api.github.com/users/octocat/hovercard?subject_type=repository&subject_id=1300192
+    } catch (err) {
+        console.log(err);
+    }
+}
